e5623b3724b1460636b66b19744601736bcf2426
MarkKoivula <mkoivula6515@gmail.com>
2020-08-22 14:22:34 -0500

    presentation scripts work

--- scripts/scripts.sql
+++ scripts/scripts.sql
@@ -12,27 +12,25 @@ WHERE CAST(replace(price,'$','') as numeric(10,2)) <= '1.00'
 GROUP BY genres
 ORDER BY 1 ASC
 
-
 --- counts of app_store apps vs. play_Store_apps <= 1.00 ------
 SELECT 'app_store_apps <= 1.00' as type,
 	    COUNT(*)
 FROM public.app_store_apps
 WHERE price <= 1.00 --4784
-
+UNION ALL
 --- counts of play_store apps <= 1.00 
 SELECT 'play_store_apps <= 1.00' as type,
 	    COUNT(*)
 FROM public.play_store_apps
 WHERE CAST(replace(price,'$','') as numeric(10,2)) <= '1.00'-- 10191
 
-
-
+UNION ALL
 --- counts of app_store apps vs. play_Store_apps > 1.00 
 SELECT 'app_store_apps > 1.00' as type,
        COUNT(*)
 FROM public.app_store_apps
 WHERE price > 1.00  -- 2413
-
+UNION ALL
 --- counts of play_store apps > 1.00 
 SELECT 'play_store_apps > 1.00' as type,
        COUNT(*)
@@ -40,7 +38,34 @@ FROM public.play_store_apps
 WHERE CAST(replace(price,'$','') as numeric(10,2)) >'1.00' -- 649 
 
 ----------------------------------------------------------------------------
+-- Get average user years for all apps in both stores
+-- new -- 
+SELECT z.avg_user_years
+INTO temp table alldata
+FROM (
+	  SELECT  ---- AVG(star_years * longevity) as avg_user_years for all common apps from both stores	           	  
+       CAST(AVG( 
+		     TRUNC(((a.rating * (cast(a.review_count as numeric(10,0)) * 25.5)) + 
+              (cast(replace(replace(p.install_count,'+',''),',','') as numeric (10,0)) * p.rating))/
+               ((cast(a.review_count as numeric(10,0)) * 25.5) + 
+                 cast(replace(replace(p.install_count,'+',''),',','') as numeric (10,0))) * 
+                  (cast(a.review_count as numeric(10,0)) * 25.5) + 
+                   cast(replace(replace(p.install_count,'+',''),',','') as numeric (10,0)),0) * 
+					   
+			  (TRUNC((ROUND(((a.rating * (cast(a.review_count as numeric(10,0)) * 25.5)) + 
+                (cast(replace(replace(p.install_count,'+',''),',','') as numeric (10,0)) * p.rating))/
+                 ((cast(a.review_count as numeric(10,0)) * 25.5) + 
+                   cast(replace(replace(p.install_count,'+',''),',','') as numeric (10,0)))*2,0)/2),1) * 2) + 1) as numeric(12,2)) as  avg_user_years
+	      
+     FROM play_store_apps as p
+       INNER JOIN app_store_apps as a
+        ON p.name = a.name
+     
+ ) z
 
+--drop table alldata
+
+SELECT * FROM alldata --1,061,854,675.67
 
 WITH x AS (
 	       SELECT p.name, 
@@ -78,7 +103,7 @@ WITH x AS (
 	      CAST(((((TRUNC((ROUND(((a.rating * (cast(a.review_count as numeric(10,0)) * 25.5)) + 
             (cast(replace(replace(p.install_count,'+',''),',','') as numeric (10,0)) * p.rating))/
             ((cast(a.review_count as numeric(10,0)) * 25.5) + 
-            cast(replace(replace(p.install_count,'+',''),',','') as numeric (10,0)))*2,0)/2),1) * 2) + 1) * 12) * 5000) as numeric(12,2)) as total_gross_profit,
+            cast(replace(replace(p.install_count,'+',''),',','') as numeric (10,0)))*2,0)/2),1) * 2) + 1) * 12) * 2500) as numeric(12,2)) as total_gross_profit,
 	
 	      CAST(1000 as numeric(10,2)) as total_marketing_cost,
 	
@@ -86,7 +111,6 @@ WITH x AS (
             (cast(replace(replace(p.install_count,'+',''),',','') as numeric (10,0)) * p.rating))/
             ((cast(a.review_count as numeric(10,0)) * 25.5) + 
             cast(replace(replace(p.install_count,'+',''),',','') as numeric (10,0)))*2,0)/2),1) * 2) + 1) * 12) * 5000) - 1000) as numeric(12,2)) as total_Net
-	
 FROM play_store_apps as p
 INNER JOIN app_store_apps as a
 ON p.name = a.name
@@ -96,47 +120,16 @@ WHERE   a.price <= 1 and
  )
 
 
--- select * from x
-
-
 SELECT name, 
-	   genres,
 	  total_reviews, 
 	  total_installs, 
 	  user_stars, 
 	  star_years, 
 	  longevity, 
-	  star_years * longevity as user_years,
-	  total_purchase_price,
-	  total_gross_profit,
-	  total_marketing_cost,
-	  total_Net,
-	  RANK() OVER(ORDER BY total_installs, longevity DESC)
-	  
+	  star_years * longevity as user_years
 FROM x
 WHERE rn = 1
 ORDER BY user_years desc;
-
-
--- Count of App store apps by Genre -- 	
-SELECT 'play store apps' as type,
-       genres, 
-	   COUNT(*) as Total
-FROM public.play_store_apps
-GROUP BY genres
-ORDER BY Total DESC
-
--- Count of Play store Approx. users, review count by app -- 
-SELECT 'play store app' as title,
-       a.name,
-       SUM(cast(a.review_count as numeric(10,0)) * 25.5) as ApproxUserCnt,
-       SUM(review_count),
-	   ROW_NUMBER() OVER (Partition by a.name) as rn
-FROM public.play_store_apps a
-GROUP BY a.name
-ORDER BY 3 DESC
-
-
 
 
 
